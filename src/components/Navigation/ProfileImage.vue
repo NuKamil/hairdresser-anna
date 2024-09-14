99c26007b216dd3fd3affda3bfd9bfeebd6f1c53
kamil <kamill1983@gmail.com>
2024-09-14 22:59:46 +0200

    Working on shoping and favorite icon on profile image component

--- src/components/Navigation/ProfileImage.vue
+++ src/components/Navigation/ProfileImage.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="flex flex-col text-sm">
-    <button
-      @click="toggleDropdown"
-      class="border-1 relative rounded-full border-solid border-kamil-orange-dark bg-kamil-orange-dark p-1 transition-shadow duration-300 hover:shadow-blue"
-    >
-      <img :src="image" alt="" class="h-12 w-12 rounded-full object-contain object-center" />
-    </button>
+  <div class="flex h-full items-center justify-center">
+    <div class="mr-3 flex h-full items-center text-2xl text-kamil-blue-dark">
+      <div class="mx-3 flex">
+        <button class="mr-2">
+          <font-awesome-icon :icon="['fas', 'heart']" class="text-kamil-orange-dark" />
+        </button>
+        <button class="relative shadow-kamil-blue-light">
+          <font-awesome-icon :icon="['fas', 'bag-shopping']" class="text-kamil-orange-dark" />
+          <span
+            class="absolute left-3 top-0 flex h-5 w-5 items-center justify-center rounded-full bg-kamil-blue-light text-xs"
+          >
+            3
+          </span>
+        </button>
+      </div>
+      <span class="cursor-default">123,40</span>
+    </div>
+    <div class="flex flex-col text-sm">
+      <button
+        @click="toggleDropdown"
+        class="border-1 relative rounded-full border-solid border-kamil-orange-dark bg-kamil-orange-dark p-1 transition-shadow duration-300 hover:shadow-blue"
+      >
+        <img :src="image" alt="" class="h-8 w-8 rounded-full object-contain object-center" />
+      </button>
 
-    <!-- Dropdown -->
-    <dropdown v-show="isOpen">
-      <template #extra-button>
-        <action-button :is-logged="props.isLogged" @click="handleClick" />
-      </template>
-    </dropdown>
+      <!-- Dropdown -->
+      <dropdown v-show="isOpen">
+        <template #extra-button>
+          <action-button :is-logged="props.isLogged" @click="handleClick" />
+        </template>
+      </dropdown>
+    </div>
   </div>
 </template>
 
@@ -20,6 +38,7 @@
 import { ref } from "vue";
 import Dropdown from "@/components/Shared/Dropdown.vue";
 import ActionButton from "@/components/Shared/ActionButton.vue";
+import { icon } from "@fortawesome/fontawesome-svg-core";
 
 const image = ref("/Pictures/20240907_102924.jpg");
 
