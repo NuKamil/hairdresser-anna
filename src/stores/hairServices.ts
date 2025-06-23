540f3c1c777676f6a582487fa5089b92b9873358
Kamil Nurkowski <kamill1983@gmail.com>
2025-06-23 14:06:14 +0200

    Coraz bliżej i bliżej!!!!

--- src/stores/hairServices.ts
+++ src/stores/hairServices.ts
@@ -7,50 +7,50 @@ export const FETCH_HAIR_SERVICES: string = "FETCH_HAIR_SERVICES";
 export const CREATE_HAIR_SERVICES: string = "CREATE_HAIR_SERVICES";
 
 export type HairServiceState = {
-  hairServices: HairService[];
+	hairServices: HairService[];
 }
 
 export const useHairServicesStore = defineStore("hairServices", {
-  state: (): HairServiceState => ({
-    hairServices: [] as HairService[],
-  }),
-  actions: {
-    async [FETCH_HAIR_SERVICES]() {
-      this.hairServices = [];
-      try {
-        const data: HairService[] = await getHairServices();
-        this.hairServices = data;
-      } catch (e) {
-        console.error("Nie udało się pobrać usług:", e);
-      }
-    },
-    async [CREATE_HAIR_SERVICES](payload: {
-      name: string;
-      price: number;
-      duration: number;
-      entityTypeId: string;
-      serviceTypeId: string;
-      isActive: boolean;
-      description?: string;
-    }) {
-      // wywołanie POST do backendu
-      await axios.post(
-        `${import.meta.env.VITE_APP_API_URL}/hair-services`,
-        {
-          Name: payload.name,
-          Price: payload.price,
-          Duration: payload.duration,
-          EntityTypeId: payload.entityTypeId,
-          ServiceTypeId: payload.serviceTypeId,
-          IsActive: payload.isActive,
-          Description: payload.description,
-        }
-      );
-      // po sukcesie odśwież listę
-      await this[FETCH_HAIR_SERVICES]();
-    },
-  },
-  getters: {
-    count: (state) => state.hairServices.length,
-  },
+	state: (): HairServiceState => ({
+		hairServices: [] as HairService[],
+	}),
+	actions: {
+		async [FETCH_HAIR_SERVICES]() {
+			this.hairServices = [];
+			try {
+				const data: HairService[] = await getHairServices();
+				this.hairServices = data;
+			} catch (e) {
+				console.error("Nie udało się pobrać usług:", e);
+			}
+		},
+		async [CREATE_HAIR_SERVICES](payload: {
+			name: string;
+			price: number;
+			duration: number;
+			entityTypeId: string;
+			serviceTypeId: string;
+			isActive: boolean;
+			description?: string;
+		}) {
+			// wywołanie POST do backendu
+			await axios.post(
+				`${import.meta.env.VITE_APP_API_URL}/hair-services`,
+				{
+					Name: payload.name,
+					Price: payload.price,
+					Duration: payload.duration,
+					EntityTypeId: payload.entityTypeId,
+					ServiceTypeId: payload.serviceTypeId,
+					IsActive: payload.isActive,
+					Description: payload.description,
+				}
+			);
+			// po sukcesie odśwież listę
+			await this[FETCH_HAIR_SERVICES]();
+		},
+	},
+	getters: {
+		count: (state) => state.hairServices.length,
+	},
 });
